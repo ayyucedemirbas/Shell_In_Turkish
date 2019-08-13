93e129dca544bf3b5210b4b631c7196f29a57925
Ayyuce Demirbas <ayyucedemirbas@windowslive.com>
2019-08-12 17:11:43 -0700

    Add files via upload

---
+++ deneme.c
@@ -0,0 +1,23 @@
+/**
+ * Shell in Turkish Ornek Programi
+ * 08.13.2019
+ * Ayyuce Demirbas 
+ * ayyuce@ieee.org
+ * 
+ * This is a five minute project just for fun
+ * Eglence amaciyla 5dk'da yazilmis bir Turkce Shell projesidir :)
+ * This code licensed under GNU GENERAL PUBLIC LICENSE Version 3
+ * 
+ * 
+ * 
+ */
+
+
+#include "shell_in_turkish.h"
+
+GIRIS { //BASLA ve BITIR komutlari calismiyor help me
+    BESDON { 
+        YAZ("Merhaba Turkce Shell :) \n"); 
+        
+    }
+    }
---
+++ shell_in_turkish.h
@@ -0,0 +1,50 @@
+
+/**
+ * Shell in Turkish
+ * 08.13.2019
+ * Ayyuce Demirbas 
+ * ayyuce@ieee.org
+ * 
+ * This is a five minute project just for fun
+ * Eglence amaciyla 5dk'da yazilmis bir Turkce Shell projesidir :)
+ * This code licensed under GNU GENERAL PUBLIC LICENSE Version 3
+ * 
+ * 
+ * 
+ */
+
+
+#define DURGUN static
+#define DIS extern
+#define TIP typedef
+#define YAPI struct
+#define BIRLIK union
+#define KAYITCI register
+
+#define TAMSAYI int
+#define KARAKTER char
+
+#define EGER if(
+#define SONRA ){
+#define YOKSA } else {
+#define YOKEGER } else if(
+#define KAPAT ;}
+#define YAZ printf
+#define ALTAGEC \n
+#define GIRIS int main()
+
+#define BASLA {
+#define BITIR }
+#define TIRNAK "
+#define DEGISTIR switch(
+#define IN ){
+#define ICIN for(
+#define IKEN while(
+#define YAP do{
+#define IKENYAP }while(
+#define SON );
+#define SONSUZDONGU for(;;){
+#define BESDON for(int i=0;i<5;i++)
+#define NOKTALIVIRGUL ;
+#define KES break
+#define DURUM case
